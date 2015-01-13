00122320fb83bb00ceb3211d411e2729f7036eee
Lysergic-Acid <caioschnepper@gmail.com>
2015-01-13 01:11:16 -0200

    Add RIL subclass for GSM

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -102,6 +102,7 @@ BOARD_USE_YAMAHA_MC1N2_AUDIO := true
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6260
+BOARD_RIL_CLASS := ../../../hardware/samsung/ril
 
 # Camera
 BOARD_USES_PROPRIETARY_LIBFIMC := true
