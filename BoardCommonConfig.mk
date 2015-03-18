0ec11a662bd0a3bceb4691fc2b77899a5d1de079
Lysergic-Acid <caioschnepper@gmail.com>
2015-03-18 16:56:23 -0300

    Enable LPM mode charging percentage

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -214,6 +214,7 @@ BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charg
 BOARD_BATTERY_DEVICE_NAME := "battery"
 BOARD_CHARGER_RES := device/samsung/galaxys2-common/res/charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/galaxys2-common/shbootimg.mk
 BOARD_USES_LEGACY_MMAP := true
 
